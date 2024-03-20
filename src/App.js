0b80ccc9703b9b64a7614f08bc09d8492dc17f2f
Bennygit123 <119126336+Bennygit123@users.noreply.github.com>
2024-03-20 22:50:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route as Route6, Switch } from 'react-router';
+
+import Navbar from './components/Navbar';
+import EmployeeDashboard from './components/EmployeeDashboard';
+import EmployeeForm from './components/EmployeeForm';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<EmployeeDashboard />} />
+          <Route6 path="/employee-form" element={<EmployeeForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/EmployeeForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const EmployeeForm = () => {
+  return (
+    <div>
+      <h1>Employee Form</h1>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" />
+        <br />
+        <label htmlFor="designation">Designation:</label>
+        <input type="text" id="designation" name="designation" />
+        <br />
+        <label htmlFor="location">Location:</label>
+        <input type="text" id="location" name="location" />
+        <br />
+        <label htmlFor="salary">Salary:</label>
+        <input type="text" id="salary" name="salary" />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default EmployeeForm;
